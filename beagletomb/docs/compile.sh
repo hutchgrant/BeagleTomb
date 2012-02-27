d411b69f58e51f552812a2e298f7847370a483e4
Grant Hutchinson <h.g.utchinson@gmail.com>
2012-02-27 05:29:41 -0500

    fixing file structure

--- beagletomb/docs/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod -R 777 /opt/extras.ubuntu.com/beagletomb
-cd /opt/extras.ubuntu.com/beagletomb
-qmake
-make
-
-ln -fs /opt/extras.ubuntu.com/beagletomb/beagletomb /usr/bin/beagletomb
